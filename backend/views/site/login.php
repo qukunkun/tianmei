12c1a8788dec6f024a660d5c4670abc26564ab89
qukun <qukun_i@163.com>
2018-12-05 17:25:32 +0800

    [e] 新增登录页面

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -1,29 +1,48 @@
 <?php
-use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \common\models\LoginForm */
 
-$this->title = 'Login';
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = '天美后台管理系统';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="middle-box text-center loginscreen  animated fadeInDown">
+    <div>
+        <div>
 
-    <p>Please fill out the following fields to login:</p>
+            <h5 class="logo-name">美</h5>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <?= $form->field($model, 'username') ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+        </div>
+        <h3>欢迎来到后台</h3>
+
+
+        <div class="row">
+            <div class="">
+                <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label('用户名') ?>
+
+                <?= $form->field($model, 'password')->passwordInput()->label('密码') ?>
+
+                <?= $form->field($model, 'rememberMe')->checkbox()->label('记住我') ?>
+
                 <div class="form-group">
                     <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
                 </div>
-            <?php ActiveForm::end(); ?>
+
+                <?php ActiveForm::end(); ?>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="">Copyright &copy; 2012-2016 <a href="http://www.tianmei.shop/" target="_blank">tianmei.shop</a>
         </div>
     </div>
 </div>
+
+
+
